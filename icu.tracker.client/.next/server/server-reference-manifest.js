416a2919f2d6331e1c49d5b5d85a26bbf0a321cc
Abel Bekele <95418375+switch247@users.noreply.github.com>
2024-11-29 16:10:14 +0300

    Revert "init"
    This reverts commit 46153714d4e9d07596a059ffd5016d93ba8f4229.

--- icu.tracker.client/.next/server/server-reference-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"YC9ZguwU5IbDvlGQzqAv34qYzlUZ6fnZlg4iZxjVlUs=\"\n}"
--- icu.tracker.client/components/index.js
+++
@@ -1,6 +0,0 @@
-"use client"
-import dynamic from "next/dynamic";
-
-const Map = dynamic(() => import("./EthiopiaHospitalsMap"), { ssr: false });
-
-export default Map;
