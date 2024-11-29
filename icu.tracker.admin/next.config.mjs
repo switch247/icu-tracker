e44f9851eb0d592dc592587f6fc86be6106cbe16
Abel Bekele <95418375+switch247@users.noreply.github.com>
2024-11-29 19:05:25 +0300

    Enable React strict mode in Next.js configuration

--- icu.tracker.admin/next.config.mjs
+++ icu.tracker.admin/next.config.mjs
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   pageExtensions: ['tsx', 'ts', 'jsx', 'js'],
+  reactStrictMode: true,
   // distDir: 'build',
 };
 // module.exports = nextConfig;
